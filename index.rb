e58fd0ea8619277ebf9e199312760c2b806282a8
Jared Haralson <jharalson29@gmail.com>
2022-05-27 19:38:14 -0400

    first Ruby commit

---
+++ index.rb
@@ -0,0 +1,16 @@
+# Lambda vs Proc
+def batman_ironman_proc
+    victor = Proc.new { return "Batman will win!" }
+    victor.call
+    "Iron Man will win!"
+  end
+  
+  puts batman_ironman_proc
+  
+  def batman_ironman_lambda
+    victor = lambda { return "Batman will win!" }
+    victor.call
+    "Iron Man will win!"
+  end
+  
+  puts batman_ironman_lambda
